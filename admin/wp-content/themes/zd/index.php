f9c8eca2ff99b391d44682bbfdd0584b18df3fc4
diogolean <diogolean@gmail.com>
2013-09-05 00:53:54 -0300

    Fundo branco p passa passa closes #7

--- admin/wp-content/themes/zd/index.php
+++ admin/wp-content/themes/zd/index.php
@@ -11,10 +11,13 @@
 		<div id="imgs-container">
 			<div id="img-oppened"></div>
 		</div>
+		<div id="bgPassador"></div>
 	</div>
 
 	<div class="footer container container-fluid" id="rodape">
-		<div id="imgRODAPE"></div>
+		<div id="imgs-containerFOOTER">
+			<div id="imgRODAPE"></div>
+		</div>
 	</div>
 </div>
 
