2b40c470ab91453bbce96b7f0911a59e93197e70
diogolean <diogolean@gmail.com>
2013-10-18 17:58:09 -0300

    remove segundo fundo

--- wp-content/themes/zd/index.php
+++ wp-content/themes/zd/index.php
@@ -8,7 +8,6 @@ get_header(); ?>
 <div class="container content container-fluid" id="home">
 	<div class="row-fluid">
 		<div id="imgBG"></div>
-		<div id="imgBG2"></div>
 	</div>
 
 	<div class="row-fluid" id="colecao">
